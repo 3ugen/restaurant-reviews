ad7c46676cd836c1bb4388fd071bf653082dc911
Eugene B <enbotnaru@gmail.com>
2018-08-13 16:24:18 -0700

    Will switch to google maps api

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -19,7 +19,7 @@ class DBHelper {
   static fetchRestaurants(callback) {
     let xhr = new XMLHttpRequest();
     xhr.open('GET', DBHelper.DATABASE_URL);
-    xhr.onload = ()=>{
+    xhr.onload = () => {
       if (xhr.status === 200) {
         // Got a success response from server!
         const json = JSON.parse(xhr.responseText);
@@ -30,8 +30,7 @@ class DBHelper {
         const error = (`Request failed. Returned status of ${xhr.status}`);
         callback(error, null);
       }
-    }
-    ;
+    };
     xhr.send();
   }
 
@@ -40,11 +39,11 @@ class DBHelper {
    */
   static fetchRestaurantById(id, callback) {
     // fetch all restaurants with proper error handling.
-    DBHelper.fetchRestaurants((error,restaurants)=>{
+    DBHelper.fetchRestaurants((error, restaurants) => {
       if (error) {
         callback(error, null);
       } else {
-        const restaurant = restaurants.find(r=>r.id == id);
+        const restaurant = restaurants.find(r => r.id == id);
         if (restaurant) {
           // Got the restaurant
           callback(null, restaurant);
@@ -53,8 +52,7 @@ class DBHelper {
           callback('Restaurant does not exist', null);
         }
       }
-    }
-    );
+    });
   }
 
   /**
@@ -62,16 +60,15 @@ class DBHelper {
    */
   static fetchRestaurantByCuisine(cuisine, callback) {
     // Fetch all restaurants  with proper error handling
-    DBHelper.fetchRestaurants((error,restaurants)=>{
+    DBHelper.fetchRestaurants((error, restaurants) => {
       if (error) {
         callback(error, null);
       } else {
         // Filter restaurants to have only given cuisine type
-        const results = restaurants.filter(r=>r.cuisine_type == cuisine);
+        const results = restaurants.filter(r => r.cuisine_type == cuisine);
         callback(null, results);
       }
-    }
-    );
+    });
   }
 
   /**
@@ -79,16 +76,15 @@ class DBHelper {
    */
   static fetchRestaurantByNeighborhood(neighborhood, callback) {
     // Fetch all restaurants
-    DBHelper.fetchRestaurants((error,restaurants)=>{
+    DBHelper.fetchRestaurants((error, restaurants) => {
       if (error) {
         callback(error, null);
       } else {
         // Filter restaurants to have only given neighborhood
-        const results = restaurants.filter(r=>r.neighborhood == neighborhood);
+        const results = restaurants.filter(r => r.neighborhood == neighborhood);
         callback(null, results);
       }
-    }
-    );
+    });
   }
 
   /**
@@ -96,23 +92,22 @@ class DBHelper {
    */
   static fetchRestaurantByCuisineAndNeighborhood(cuisine, neighborhood, callback) {
     // Fetch all restaurants
-    DBHelper.fetchRestaurants((error,restaurants)=>{
+    DBHelper.fetchRestaurants((error, restaurants) => {
       if (error) {
         callback(error, null);
       } else {
         let results = restaurants
         if (cuisine != 'all') {
           // filter by cuisine
-          results = results.filter(r=>r.cuisine_type == cuisine);
+          results = results.filter(r => r.cuisine_type == cuisine);
         }
         if (neighborhood != 'all') {
           // filter by neighborhood
-          results = results.filter(r=>r.neighborhood == neighborhood);
+          results = results.filter(r => r.neighborhood == neighborhood);
         }
         callback(null, results);
       }
-    }
-    );
+    });
   }
 
   /**
@@ -120,18 +115,17 @@ class DBHelper {
    */
   static fetchNeighborhoods(callback) {
     // Fetch all restaurants
-    DBHelper.fetchRestaurants((error,restaurants)=>{
+    DBHelper.fetchRestaurants((error, restaurants) => {
       if (error) {
         callback(error, null);
       } else {
         // Get all neighborhoods from all restaurants
-        const neighborhoods = restaurants.map((v,i)=>restaurants[i].neighborhood)
+        const neighborhoods = restaurants.map((v, i) => restaurants[i].neighborhood)
         // Remove duplicates from neighborhoods
-        const uniqueNeighborhoods = neighborhoods.filter((v,i)=>neighborhoods.indexOf(v) == i)
+        const uniqueNeighborhoods = neighborhoods.filter((v, i) => neighborhoods.indexOf(v) == i)
         callback(null, uniqueNeighborhoods);
       }
-    }
-    );
+    });
   }
 
   /**
@@ -139,18 +133,17 @@ class DBHelper {
    */
   static fetchCuisines(callback) {
     // Fetch all restaurants
-    DBHelper.fetchRestaurants((error,restaurants)=>{
+    DBHelper.fetchRestaurants((error, restaurants) => {
       if (error) {
         callback(error, null);
       } else {
         // Get all cuisines from all restaurants
-        const cuisines = restaurants.map((v,i)=>restaurants[i].cuisine_type)
+        const cuisines = restaurants.map((v, i) => restaurants[i].cuisine_type)
         // Remove duplicates from cuisines
-        const uniqueCuisines = cuisines.filter((v,i)=>cuisines.indexOf(v) == i)
+        const uniqueCuisines = cuisines.filter((v, i) => cuisines.indexOf(v) == i)
         callback(null, uniqueCuisines);
       }
-    }
-    );
+    });
   }
 
   /**
@@ -167,7 +160,7 @@ class DBHelper {
   //     return (`/img/${restaurant.photograph}`);
   //   }
   static imageUrlForRestaurant(restaurant, type) {
-    if (!type){
+    if (!type) {
       return (`/img/${restaurant.photograph}`);
     }
     if (restaurant.photograph) {
@@ -179,25 +172,25 @@ class DBHelper {
   /**
    * Map marker for a restaurant.
    */
+  // static mapMarkerForRestaurant(restaurant, map) {
+  //   // https://leafletjs.com/reference-1.3.0.html#marker  
+  //   const marker = new L.marker([restaurant.latlng.lat, restaurant.latlng.lng],{
+  //     title: restaurant.name,
+  //     alt: restaurant.name,
+  //     url: DBHelper.urlForRestaurant(restaurant)
+  //   })
+  //   marker.addTo(newMap);
+  //   return marker;
+  // }
   static mapMarkerForRestaurant(restaurant, map) {
-    // https://leafletjs.com/reference-1.3.0.html#marker  
-    const marker = new L.marker([restaurant.latlng.lat, restaurant.latlng.lng],{
-      title: restaurant.name,
-      alt: restaurant.name,
-      url: DBHelper.urlForRestaurant(restaurant)
-    })
-    marker.addTo(newMap);
-    return marker;
-  }
-  /* static mapMarkerForRestaurant(restaurant, map) {
     const marker = new google.maps.Marker({
       position: restaurant.latlng,
       title: restaurant.name,
       url: DBHelper.urlForRestaurant(restaurant),
       map: map,
-      animation: google.maps.Animation.DROP}
-    );
+      animation: google.maps.Animation.DROP
+    });
     return marker;
-  } */
+  }
 
 }
--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -11,31 +11,31 @@ document.addEventListener('DOMContentLoaded', (event) => {
 /**
  * Initialize leaflet map
  */
-initMap = () => {
-  fetchRestaurantFromURL((error, restaurant) => {
-    if (error) { // Got an error!
-      console.error(error);
-    } else {
-      self.newMap = L.map('map', {
-        center: [restaurant.latlng.lat, restaurant.latlng.lng],
-        zoom: 16,
-        scrollWheelZoom: false
-      });
-      L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-        mapboxToken: 'pk.eyJ1IjoiM3VnZW5lIiwiYSI6ImNqazZlMTBraDB5c20zd3Fsa3gyeTJiOGkifQ.5-WHJFWbNpbvVj_QiZjpNg',
-        maxZoom: 18,
-        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
-          '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
-          'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-        id: 'mapbox.streets'
-      }).addTo(newMap);
-      fillBreadcrumb();
-      DBHelper.mapMarkerForRestaurant(self.restaurant, self.newMap);
-    }
-  });
-}
+// initMap = () => {
+//   fetchRestaurantFromURL((error, restaurant) => {
+//     if (error) { // Got an error!
+//       console.error(error);
+//     } else {
+//       self.newMap = L.map('map', {
+//         center: [restaurant.latlng.lat, restaurant.latlng.lng],
+//         zoom: 16,
+//         scrollWheelZoom: false
+//       });
+//       L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
+//         mapboxToken: 'pk.eyJ1IjoiM3VnZW5lIiwiYSI6ImNqazZlMTBraDB5c20zd3Fsa3gyeTJiOGkifQ.5-WHJFWbNpbvVj_QiZjpNg',
+//         maxZoom: 18,
+//         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
+//           '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+//           'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+//         id: 'mapbox.streets'
+//       }).addTo(newMap);
+//       fillBreadcrumb();
+//       DBHelper.mapMarkerForRestaurant(self.restaurant, self.newMap);
+//     }
+//   });
+// }
 
-/* window.initMap = () => {
+window.initMap = () => {
   fetchRestaurantFromURL((error, restaurant) => {
     if (error) { // Got an error!
       console.error(error);
@@ -49,7 +49,7 @@ initMap = () => {
       DBHelper.mapMarkerForRestaurant(self.restaurant, self.map);
     }
   });
-} */
+}
 
 /**
  * Get current restaurant from page URL.
@@ -170,7 +170,7 @@ createReviewHTML = (review) => {
 /**
  * Add restaurant name to the breadcrumb navigation menu
  */
-fillBreadcrumb = (restaurant=self.restaurant) => {
+fillBreadcrumb = (restaurant = self.restaurant) => {
   const breadcrumb = document.getElementById('breadcrumb');
   const li = document.createElement('li');
   li.innerHTML = restaurant.name;
