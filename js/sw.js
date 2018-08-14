2c16f02132876dd5f4ab6233ef3e81dd043d98c8
Eugene B <enbotnaru@gmail.com>
2018-08-14 12:36:28 -0700

    sw.js: add install event listner

--- js/sw.js
+++ js/sw.js
@@ -1,1 +1,9 @@
 let cashName = "rr-local-v1";
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(staticCacheName).then(cache => {
+            return cache.addAll(['/']);
+        }).catch(err => console.log('caching failed', err))
+    );
+});
