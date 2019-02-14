bec128e3363d450df0d7fc737ba38f6457c78e5a
Eugeniu Botnaru <enbotnaru@gmail.com>
2019-02-14 16:01:14 +0000

    Update dbhelper.js

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -10,7 +10,8 @@ class DBHelper {
   static get DATABASE_URL() {
 //     const port = 80
     // Change this to your server port
-    return `http://ebv-gh.github.io/restaurant-reviews:${port}/data/restaurants.json`;
+//     return `http://ebv-gh.github.io/restaurant-reviews:${port}/data/restaurants.json`;
+    return `http://ebv-gh.github.io/restaurant-reviews/data/restaurants.json`;
   }
 
   /**
